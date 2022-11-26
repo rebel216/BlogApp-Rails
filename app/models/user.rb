4682009fcc8e30240fecb67d8086383708f70fc6
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-11-26 17:38:10 +0530

    Added rpecs and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id, dependent: :destroy
-  has_many :comments, foreign_key: :author_id, dependent: :destroy
-  has_many :likes, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates_numericality_of :post_counter, only_integer: true, greater_than_or_equal_to: 0
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(5)
   end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations for User model' do
+    before(:each) do
+      @user = User.new(name: 'Tom', bio: 'Teacher from Mexico', post_counter: 0)
+    end
+
+    before { @user.save }
+
+    it 'if there is name' do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'PostCounter must be greater than or equal to zero' do
+      @user.post_counter = -1
+      expect(@user).to_not be_valid
+    end
+
+    it 'PostCounter must be greater than or equal to zero' do
+      @user.post_counter = 5
+      expect(@user).to be_valid
+    end
+  end
+end
