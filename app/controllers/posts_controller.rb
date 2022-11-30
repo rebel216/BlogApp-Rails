1201f0fe469a35577e712d1e98eef699fa4de4fa
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-01 00:16:15 +0530

    Added controllers Specs tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @author = User.find(params[:user_id])
+    @post = @author.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_path(id: @post.author_id), notice: 'Post was successfully created'
+
+    else
+      render :new, alert: 'Error ccurred while creating the post'
+    end
+  end
+
   private
 
   def post_params
