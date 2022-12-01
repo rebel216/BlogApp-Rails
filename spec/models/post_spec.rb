d65b793312d4541a7c9c43e5d9e2bb7c482e181e
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-12-01 19:20:33 +0530

    Added Posts show and index specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'For the Post model' do
     before(:each) do
-      @user = User.new(name: 'Tom', photo: 'image.png', bio: 'Teacher from Mexico', post_counter: 0)
+      @user = User.new(name: 'Tom', bio: 'Teacher from Mexico', post_counter: 0)
       @post = Post.new(author: @user, title: 'Test', text: 'testing', likes_counter: 6, comments_counter: 1)
     end
 
@@ -33,28 +33,10 @@ RSpec.describe Post, type: :model do
       @post.comments_counter = -5
       expect(@post).to_not be_valid
     end
-
+    
     it 'if comments counter is integer' do
       @post.comments_counter = 8
       expect(@post).to be_valid
-    end
-
-    it 'recent_comments method should return the last five comments' do
-      user = User.new
-
-      post = described_class.create(title: 'Post One', text: 'This is the post one')
-
-      post.comments = [
-        Comment.new({ author: user, text: 'This is the comment one' }),
-        Comment.new({ author: user, text: 'This is the comment two' }),
-        Comment.new({ author: user, text: 'This is the comment three' }),
-        Comment.new({ author: user, text: 'This is the comment four' }),
-        Comment.new({ author: user, text: 'This is the comment five' }),
-        Comment.new({ author: user, text: 'This is the comment six' })
-      ]
-
-      expect(post.recent_comments.size).to be(5)
-      expect(post.recent_comments.pluck(:id)).to match_array(post.comments.last(5).pluck(:id))
     end
   end
 end
--- spec/requests/posts_specs.rb
+++ spec/requests/posts_specs.rb
@@ -1,60 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe 'For the Post model' do
-    before(:each) do
-      @user = User.new(name: 'Tom', photo: 'image.png', bio: 'Teacher from Mexico', post_counter: 0)
-      @post = Post.new(author: @user, title: 'Test', text: 'testing', likes_counter: 6, comments_counter: 1)
+RSpec.describe 'Posts', type: :request do
+  context 'the index method' do
+    it 'show all users' do
+      get('/users/10/posts')
+      expect(assigns(:posts)).to eq(Post.all)
     end
 
-    before { @post.save }
-
-    it 'if there is title' do
-      @post.title = true
-      expect(@post).to be_valid
+    it 'render index template' do
+      get('/users/10/posts')
+      expect(response).to render_template('index')
     end
 
-    it 'if there is max 250 characters' do
-      @post.title = 'Testing'
-      expect(@post).to be_valid
+    it 'the veiw body is correct (index)' do
+      get('/users/10/posts')
+      expect(response.body).to include('Index for user posts')
+    end
+  end
+
+  context 'the show method' do
+    it 'show specific user post veiw' do
+      get('/users/403/posts/1')
+      expect(response.body).to include('specific user post')
     end
 
-    it 'if likes counter is integer' do
-      @post.likes_counter = 5
-      expect(@post).to be_valid
-    end
-
-    it 'if likes counter greater than or equal to zero' do
-      @post.likes_counter = -9
-      expect(@post).to_not be_valid
-    end
-
-    it 'if comments counter greater than or equal to zero.' do
-      @post.comments_counter = -5
-      expect(@post).to_not be_valid
-    end
-
-    it 'if comments counter is integer' do
-      @post.comments_counter = 8
-      expect(@post).to be_valid
-    end
-
-    it 'recent_comments method should return the last five comments' do
-      user = User.new
-
-      post = described_class.create(title: 'Post One', text: 'This is the post one')
-
-      post.comments = [
-        Comment.new({ author: user, text: 'This is the comment one' }),
-        Comment.new({ author: user, text: 'This is the comment two' }),
-        Comment.new({ author: user, text: 'This is the comment three' }),
-        Comment.new({ author: user, text: 'This is the comment four' }),
-        Comment.new({ author: user, text: 'This is the comment five' }),
-        Comment.new({ author: user, text: 'This is the comment six' })
-      ]
-
-      expect(post.recent_comments.size).to be(5)
-      expect(post.recent_comments.pluck(:id)).to match_array(post.comments.last(5).pluck(:id))
+    it 'render the correct page' do
+      get('/users/403/posts/1')
+      expect(response.body).to render_template('show')
     end
   end
 end
